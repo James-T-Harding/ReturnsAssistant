94978d99708affc624fc1cd40f208a7ce24c0cc3
James-T-Harding <j.harding26@outlook.com>
2020-02-24 00:17:45 +0000

    Added files.

---
+++ Classes/DateRange.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tax_Returns_Assistant
+{
+    public sealed class DateFilter : FilterOpts
+    {
+        public DateTime Sdate, Edate;
+        public int colnum;
+
+        public DateFilter(int colnum,DateTime Sdate,DateTime Edate)
+        {
+            this.colnum = colnum;
+
+            this.Sdate = Sdate;
+            this.Edate = Edate;
+        }
+
+        public override void Filter(ref string[][] vals)
+        {
+            vals = Array.FindAll(vals,
+                x => Utility.ReadDate(x[colnum]) >= Sdate && Utility.ReadDate(x[colnum]) <= Edate);
+        }
+
+    }
+}
---
+++ Classes/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Tax_Returns_Assistant
+{
+    static class Utility
+    {
+        static public DateTime ReadDate(string inp)
+        {
+            string[] DateArr = inp.Trim().Split('/');
+
+            int day = Convert.ToInt32(DateArr[0]);
+            int month = Convert.ToInt32(DateArr[1]);
+
+            string years = DateArr[2].Length == 2 ? "20" + DateArr[2] : DateArr[2];
+
+            int year = Convert.ToInt32(years);
+
+            return new DateTime(year, month, day);
+        }
+    }
+}
